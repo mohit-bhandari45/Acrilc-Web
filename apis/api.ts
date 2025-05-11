ff0e4efb14463c5b20095c5878bf1d7053e6b5ec
Mohit  Bhandari <151373021+mohit-bhandari45@users.noreply.github.com>
2025-05-11 06:50:14 +0530

    Update api.ts

--- apis/api.ts
+++ apis/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // const BASE_URL: string = "https://joyous-reeta-mohit123-198cb6c8.koyeb.app"
 // const BASE_URL: string = "https://acrilc-backend-production.up.railway.app"
 // const BASE_URL: string = "http://localhost:8000"
-const BASE_URL: string = "https://13.202.135.126/:8000"
+const BASE_URL: string = "https://api.acrilc.com"
 
 const api = axios.create({
     baseURL: BASE_URL,
