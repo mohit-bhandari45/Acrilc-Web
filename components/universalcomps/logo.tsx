ac2c765e0d7933bdf1ffe76069e15a88d072f158
mohit-bhandari45 <mohitbhandari852@gmail.com>
2025-05-25 09:08:29 +0530

    fix: some fix

--- components/universalcomps/logo.tsx
+++ components/universalcomps/logo.tsx
@@ -1,17 +1,7 @@
-"use client";
-
-import { Poppins } from '@next/font/google';
-
-const poppins = Poppins({
-  weight: ['700', '800', '900'],
-  subsets: ['latin'],
-  display: 'swap',
-});
-
 const Logo = () => {
   return (
     <div className="relative left-[-5] text-black font-[Helvetica]">
-      <span className={`text-6xl font-bold ${poppins.className}`}>a</span>
+      <span className={`text-6xl font-bold`}>a</span>
       <span className="text-4xl font-bold">c</span>
       <span className="text-4xl font-bold">r</span>
       <span className="text-4xl font-bold">i</span>
